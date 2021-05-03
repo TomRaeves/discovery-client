54b32b74b9ab6bdb24fae7033db291c0135af0fd
tomra <tom.raeves3we@gmail.com>
2021-05-03 12:05:09 +0200

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,335 @@
+import org.w3c.dom.events.EventException;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import static java.lang.StrictMath.abs;
+
+public class Client {
+
+    //https://www.baeldung.com/java-broadcast-multicast
+
+    private static final int receivePort = 4500;
+    private static final int sendPort = 4501;
+    private static final int multiCastPort = 3456;
+    private static final String multicastAddress = "225.6.7.8"; //Dit moet nog specifiek worden
+
+    private static int amountOtherNodes = -1;
+
+    private static int previousNodeID = -1;
+    private static int currentNodeID= -1;
+    private static int nextNodeID = -1;
+
+    private static boolean running = true;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Hostname
+        System.out.println("Choose your hostname: ");
+        String hostName = sc.nextLine();
+        //hostID
+        Client.currentNodeID = hashCode(hostName); //read the name and then hash the name
+        Client.nextNodeID = Client.currentNodeID;
+        Client.previousNodeID = Client.currentNodeID;
+        //ServerIP
+        System.out.println("Give the name of the server: ");
+        String sAddress = sc.nextLine();
+        InetAddress serverAddress = null;
+        try {
+            serverAddress = InetAddress.getByName(sAddress); //find the server IP using it's name
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        //UDPHANDLER
+        UDPReceiveHandler UDPHandler = new UDPReceiveHandler(Client.receivePort);
+        UDPHandler.start();
+
+        //UDPMULTIHANDLER
+        UDPMultiReceiveHandler UDPMultiReceiveHandler = null;
+        try {
+            UDPMultiReceiveHandler = new UDPMultiReceiveHandler(hostName, InetAddress.getByName(Client.multicastAddress), Client.multiCastPort);
+        } catch (UnknownHostException err) {
+            err.printStackTrace();
+        }
+        if (UDPMultiReceiveHandler != null) {
+            UDPMultiReceiveHandler.start();
+        }
+
+        //DISCOVERY
+        try {
+            sendMultiCast("Start,"+hostName, InetAddress.getByName(Client.multicastAddress));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        while(Client.running) {
+            String command = sc.nextLine();
+            switch (command) {
+                case "Exit":
+                    String toSend = "Exit,Curr: " + Client.currentNodeID + ",Prev: " + Client.previousNodeID + ",Next: " + Client.nextNodeID;
+                    sendUnicast(toSend, serverAddress);
+                    UDPHandler.shutdown();
+                    if (UDPMultiReceiveHandler != null) {
+                        UDPMultiReceiveHandler.shutdown();
+                    }
+                    Client.running = false;
+                    break;
+                case "help":
+                    System.out.println("List of all commands:");
+                    System.out.println("Leave network: 'Exit'");
+                    System.out.println("Get hashMap of nodes on server: 'getMap'");
+                    System.out.println("Get hashMap of files on server: 'getFileMap'");
+                    System.out.println("Add file to server: 'addFile'");
+                    System.out.println("Get owner of file on server: 'getFileOwner'");
+                    break;
+
+                case "getMap":
+                    try {
+                        sendGET("getMap",sAddress);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "getFileMap":
+                    try {
+                        sendGET("getFileMap",sAddress);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "addFile":
+                    System.out.println("Give the name for the file to be added: ");
+                    String name = sc.nextLine();
+                    try {
+                        sendPUT("addFile/"+name,sAddress);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                case "getFileOwner":
+                    System.out.println("Give the name of the file you want to find the owner of: ");
+                    name = sc.nextLine();
+                    try {
+                        sendGET("getFileOwner/"+name,sAddress);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        System.out.println("Quiting program, till next time!");
+        System.exit(0);
+
+
+    }
+
+
+    public static int hashCode(String input){
+        long max = 2147483647;
+        long min = -2147483647;
+
+        long result = ((input.hashCode()+max)*32768)/(max+Math.abs(min));
+
+        //double result = (IP.hashCode() + hostname.hashCode()+max)*(32768d/(max+abs(min)));
+        //Eventueel hash maken over naam EN IP zodat je telkens een verschillende ID hebt
+
+        return (int) result;
+    }
+
+    private static void sendPUT(String command, String address) throws IOException {
+        URL url = new URL("http://"+address+":8080/"+command);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("PUT");
+        int responseCode = connection.getResponseCode();
+        if(responseCode == HttpURLConnection.HTTP_OK){
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+        }else {
+            System.out.println("PUT request failed");
+        }
+    }
+
+    private static void sendGET(String command, String address) throws IOException {
+        URL url = new URL("http://"+address+":8080/"+command);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+        if(responseCode == HttpURLConnection.HTTP_OK){
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+        }else {
+            System.out.println("GET request failed");
+        }
+    }
+
+    private static void sendMultiCast(String message, InetAddress address){
+        System.out.println("Sending multicast: ["+address+"]: "+message);
+        if (message != null) {
+            MulticastSocket UDPSocket = null; //create new socket
+            try {
+                UDPSocket = new MulticastSocket();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            byte[] buffer = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer,buffer.length, address, Client.multiCastPort);
+            try {
+                if (UDPSocket != null) {
+                    UDPSocket.send(packet);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (UDPSocket != null) {
+                UDPSocket.close();
+            }
+        }
+    }
+
+    private static void sendUnicast(String message, InetAddress adress){
+        System.out.println("Sending unicast: ["+adress+"]: "+message);
+        if (message != null){
+            DatagramSocket UDPSocket = null;
+            try {
+                UDPSocket = new DatagramSocket();
+            } catch (SocketException e) {
+                e.printStackTrace();
+            }
+            byte[] buffer = message.getBytes();
+            DatagramPacket packet = new DatagramPacket(buffer,buffer.length, adress, sendPort);
+            try {
+                if (UDPSocket != null) {
+                    UDPSocket.send(packet);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (UDPSocket != null) {
+                UDPSocket.close();
+            }
+        }
+    }
+
+    public static void update(int id, InetAddress hostAddress) {
+        Client.amountOtherNodes = Client.amountOtherNodes + 1;
+        System.out.println("New node detected with hash: "+id+" , other nodes on network: " + Client.amountOtherNodes);
+        //Als de eerste client joined in het netwerk
+        if (Client.currentNodeID == Client.nextNodeID && Client.currentNodeID == Client.previousNodeID) {
+            Client.previousNodeID = id;
+            Client.nextNodeID = id;
+        } else {
+            //Niet de eerste, maar de tweede
+            if (Client.nextNodeID == Client.previousNodeID){
+                if (Client.currentNodeID < id) {
+                    Client.nextNodeID = id;
+                    sendUnicast("I ["+Client.currentNodeID+"], have put you as my nextNode",hostAddress);
+                }
+                else {
+                    Client.previousNodeID = id;
+                    sendUnicast("I ["+Client.currentNodeID+"], have put you as my previousNode",hostAddress);
+                }
+            }
+            //Normale werking
+            else if (Client.currentNodeID < id && id < Client.nextNodeID){
+                Client.nextNodeID = id;
+                sendUnicast("I ["+Client.currentNodeID+"], have put you as my nextNode",hostAddress);
+            }
+            //Normale werking
+            else if (Client.previousNodeID < id && id < Client.currentNodeID) {
+                Client.previousNodeID = id;
+                sendUnicast("I ["+Client.currentNodeID+"], have put you as my previousNode",hostAddress);
+            }
+            //Als je op het einde van de ring zit
+            else if (Client.currentNodeID < id && Client.nextNodeID < id && Client.currentNodeID > Client.nextNodeID){
+                Client.nextNodeID = id;
+                sendUnicast("I ["+Client.currentNodeID+"], have put you as my nextNode",hostAddress);
+            }
+            //Als je in het begin van de ring zit
+            else if (Client.previousNodeID < id && Client.currentNodeID < id && Client.previousNodeID > Client.currentNodeID){
+                Client.previousNodeID = id;
+                sendUnicast("I ["+Client.currentNodeID+"], have put you as my previousNode",hostAddress);
+            }
+        }
+        System.out.println("----------------------------------------------------");
+        System.out.println("Other nodes in the network: "+Client.amountOtherNodes);
+        System.out.println("Previous ID: " + Client.previousNodeID + " || Current ID: "+Client.currentNodeID+" || Next ID: " + Client.nextNodeID);
+        System.out.println("Give a command: <help> for a list of all commands");
+        System.out.println("----------------------------------------------------");
+    }
+
+    public static void updateInitial(String message) {
+        //VB message: 3
+        //VB message: 3, Previous ID: 68465, Next ID: 321846
+        String amount;
+        String previous;
+        String next;
+        int index = message.indexOf(",");
+        if (index != -1) {
+            amount = message.substring(0, index);
+            index = (message.indexOf("Previous ID: ")) + 13;
+            int indexx = message.indexOf(", Next ID: ");
+            previous = message.substring(index, indexx);
+            index = (message.indexOf("Next ID: ")) + 9;
+            next = message.substring(index, message.length());
+            Client.amountOtherNodes = Integer.parseInt(amount);
+            Client.nextNodeID = Integer.parseInt(next);
+            Client.previousNodeID = Integer.parseInt(previous);
+        }
+        else
+            Client.amountOtherNodes = Integer.parseInt(message);
+        System.out.println("----------------------------------------------------");
+        System.out.println("Other nodes in the network: "+Client.amountOtherNodes);
+        System.out.println("Previous ID: " + Client.previousNodeID + " || Current ID: "+Client.currentNodeID+" || Next ID: " + Client.nextNodeID);
+        System.out.println("Give a command: <help> for a list of all commands");
+        System.out.println("----------------------------------------------------");
+    }
+
+    public static void exitUpdateNext(String message) {
+        String newID;
+        String exitID;
+        int index = message.indexOf(",");
+        newID = message.substring(0,index);
+        exitID = message.substring(index+1);
+        System.out.println(exitID+" sends exit, updating nextNodeID to : "+newID);
+        Client.nextNodeID = Integer.parseInt(newID);
+        System.out.println("----------------------------------------------------");
+        System.out.println("Other nodes in the network: "+Client.amountOtherNodes);
+        System.out.println("Previous ID: " + Client.previousNodeID + " || Current ID: "+Client.currentNodeID+" || Next ID: " + Client.nextNodeID);
+        System.out.println("Give a command: <help> for a list of all commands");
+        System.out.println("----------------------------------------------------");
+    }
+
+    public static void exitUpdatePrev(String message) {
+        String newID;
+        String exitID;
+        int index = message.indexOf(",");
+        newID = message.substring(0,index);
+        exitID = message.substring(index+1);
+        System.out.println(exitID+" sends exit, updating previousNodeID to : "+newID);
+        Client.nextNodeID = Integer.parseInt(newID);
+        System.out.println("----------------------------------------------------");
+        System.out.println("Other nodes in the network: "+Client.amountOtherNodes);
+        System.out.println("Previous ID: " + Client.previousNodeID + " || Current ID: "+Client.currentNodeID+" || Next ID: " + Client.nextNodeID);
+        System.out.println("Give a command: <help> for a list of all commands");
+        System.out.println("----------------------------------------------------");
+    }
+
+}
